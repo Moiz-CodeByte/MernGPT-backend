b9ac990ba1ee8d1f5c7da1b8adc181fe17ca7ebd
Abdul Moiz <moiz.codebyte@gmail.com>
2025-09-13 12:43:10 +0500

    fix typo errors

--- src/config/groq-config.ts
+++ src/config/groq-config.ts
@@ -2,7 +2,7 @@ import { Groq } from "groq-sdk";
 
 export const configureOpenAI = () => {
     const groq = new Groq({
-        apiKey: process.env.GROK_API_KEY
+        apiKey: process.env.GROQ_API_KEY
     });
     return groq;
 }
